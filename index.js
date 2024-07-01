b83e1aaa895ece8783c9aaf4b054de415affb1b5
GB <gbvaibhav@users.noreply.github.com>
2024-07-01 17:26:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_server_1 = require("./express-server");
 const server = new express_server_1.ExpressServer();
 server.start();
-//check line
